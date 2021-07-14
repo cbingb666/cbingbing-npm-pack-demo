221752bbfcd44d3d756aab34f38832cc480bd0e5
caibinghong <947694498@qq.com>
2021-07-14 09:47:35 +0800

    feat: add downCount

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # npm pack demo
+
+## 技术点
+
+- [rollup](https://www.rollupjs.com/)
+- [typescript](https://www.typescriptlang.org/)
+- [npm](https://www.npmjs.com/)
+
+### 初始化 npm
+
+```sh
+npm init
+```
+
+### rollup
+
+全局安装 rollup
+
+```sh
+npm install --global rollup
+```
+
+
+
+###
--- src/time/downCount.ts
+++ src/time/downCount.ts
@@ -1,2 +1,19 @@
-// TODO 倒数
-export default function downCount () {}
+
+type Count = number
+type Cb = (count: Count) => any
+/**
+ * 倒数
+ */
+export const downCount = async (cb: Cb, count: Count) => {
+  return new Promise<void>(resolve => {
+    cb && cb(count)
+    const timer = setInterval(() => {
+      cb && cb(count)
+      count--
+      if (count <= 0) {
+        clearInterval(timer)
+        resolve()
+      }
+    }, 1000)
+  })
+}
